56c824e177e59f7776025e52972862c632ea0f63
David Hdz <dave.hzad@gmail.com>
2021-11-13 23:32:15 -0600

    SortsExamples

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        int[] array = {30, 10, 50, 20, 60, 40};
+
+        Sort sort = new Sort();
+
+        System.out.println("BubbleSort");
+        sort.bubbleSort(array);
+        sort.printArray(array);
+        System.out.println();
+
+        System.out.println("SelectionSort");
+        sort.selectionSort(array);
+        sort.printArray(array);
+        System.out.println();
+
+
 
     }
 }
